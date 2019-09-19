0ba94506ae3149ddd63620c8d77c570857586456
Rabea Gleissner <rabeagleissner@gmail.com>
2019-09-19 19:22:08 +0800

    Remove Gatsby default artifacts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,19 +5,10 @@ import "./layout.css"
 const Layout = ({ children }) => {
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}, Built by a hungry <a href="http://rabea.dev">Rabea</a>.
         </footer>
       </div>
     </>
