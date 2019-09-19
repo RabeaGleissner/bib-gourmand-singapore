87677a25b5ed2462774125179bae9c4a0875bb88
Rabea Gleissner <rabeagleissner@gmail.com>
2019-09-19 20:26:56 +0800

    Use yaml to store restaurant info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,40 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import MapContainer from "../components/MapContainer"
 
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <section className="map">
-      <h1>The map</h1>
-      <MapContainer
-        onMount={addRestaurants(restaurants)}
-        newMapOptions={newMapOptions}
-      />
-  </section>
-</Layout>
-)
-
-const addRestaurants = (restaurants) => map => {
-  restaurants.forEach((restaurant, index) => {
-    const marker = new window.google.maps.Marker({
-      map,
-      position: restaurant.coords,
-      label: `${index + 1}`,
-      title: restaurant.name,
+const IndexPage = ({ data }) => {
+  console.log('data', data);
+  const { allRestaurantsYaml: { nodes: restaurants } } = data;
+  const addRestaurants = (restaurants) => map => {
+    restaurants.forEach((restaurant, index) => {
+      const marker = new window.google.maps.Marker({
+        map,
+        position: restaurant.coords,
+        label: restaurant.name,
+        title: restaurant.name,
+      })
+      marker.addListener('click', () => {
+        console.log('clicked on marker')
+      })
     })
-    marker.addListener('click', () => {
-      console.log('clicked on marker')
-    })
-  })
+  }
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <section className="map">
+        <h1>The map</h1>
+        <MapContainer
+          onMount={addRestaurants(restaurants)}
+          newMapOptions={newMapOptions}
+        />
+      </section>
+    </Layout>
+  )
 }
+
 
 const restaurants = [
   {
@@ -48,5 +53,21 @@ const newMapOptions = {
   },
   zoom: 15,
 };
+
+export const query = graphql`
+   query {
+     allRestaurantsYaml {
+       nodes {
+           name
+           cuisine
+           address
+           coords {
+             lat
+             lng
+           }
+       }
+     }
+   }
+ `
 
 export default IndexPage
