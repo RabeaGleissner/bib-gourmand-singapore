f638da91ca5834fe4fa7c5a78bfc14345d71772f
Rabea Gleissner <rabeagleissner@gmail.com>
2019-11-07 10:23:33 +0800

    Move navigation into header and always display it

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,43 +1,41 @@
-import React, { useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const [showNav, setShowNav] = useState(false)
-  const navIcon = showNav ? 'x' : '\u2630'
   return (
     <div>
       <header>
-        <div className="nav-container">
-          <button className="nav-icon-button" onClick={() => setShowNav(!showNav)}>{navIcon}</button>
-          {showNav && (<nav>
-            <ul>
-              <li>
-                <p><Link to="/">Home</Link></p>
-              </li>
-              <li>
-                <p><Link to="/about">More info</Link></p>
-              </li>
-              <li>
-                <p><Link to="/reviews">Restaurant reviews</Link></p>
-              </li>
-            </ul>
-          </nav>)
-          }
-        </div>
         <div className="title">
           <Link to="/"><h1>Singapore Bib Gourmand map</h1></Link>
           <p>The 58 Michelin awarded eateries on a map.</p>
+          <nav>
+            <ul>
+              <Link activeClassName="current-page" to="/">
+                <li><p>Map</p></li>
+              </Link>
+              <Link activeClassName="current-page" to="/about">
+                <li>
+                  <p>More info</p>
+                </li>
+              </Link>
+              <Link activeClassName="current-page" to="/reviews">
+                <li>
+                  <p>Reviews</p>
+                </li>
+              </Link>
+            </ul>
+          </nav>
         </div>
     </header>
     {children}
     <div className="main-container">
       <footer>
         © {new Date().getFullYear()}, Built by a hungry <a href="http://rabea.dev">Rabea</a>.
-    </footer>
+      </footer>
+    </div>
   </div>
-</div>
-)
+  )
 }
 
 export default Layout
